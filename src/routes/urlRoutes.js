7bfef49ff79f4da2770ebb1a38e99c40a9e31c0f
KIM <macbookair@MacBooks-MacBook-Air.local>
2025-12-09 19:23:49 +0100

    fixed a bug

--- src/routes/urlRoutes.js
+++ src/routes/urlRoutes.js
@@ -7,7 +7,6 @@ const { generalLimiter } = require("../middleware/rateLimiter");
 
 router.post("/shortener", protect, generalLimiter, urlController.createShortUrl);
 router.get("/shortener/me", protect, urlController.getMyUrls);
-router.get("/:code", urlController.redirectToOriginal);
 router.get("/shortener/analytics/:code", protect, urlController.getClickAnalytics);
 
 module.exports = router;
